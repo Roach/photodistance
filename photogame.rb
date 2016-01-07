84a2c6062c9163be7c65fde129e6eee44efda1b8
Roach <urbandev@gmail.com>
2016-01-07 01:59:53 -0800

    added more randomization to photo results

--- photogame.rb
+++ photogame.rb
@@ -31,7 +31,7 @@ end
 
 def get_photos(user_location)
   # Get 5 photos, extract ID, URL and GEO, then format the JSON.
-  photos = flickr.photos.search(:per_page => 100, :has_geo => true, :tags => ["landscapes"])
+  photos = flickr.photos.search(:page => rand(100), :per_page => 100, :has_geo => true, :tags => ["landscapes"])
   with_geo_data = photos.to_a.sample(5).map do |p|
     photo_info = flickr.photos.getInfo(:photo_id => p.id)
     photo_geo = photo_info.location
