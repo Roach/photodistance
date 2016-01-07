ec4dd0ea2a7a4e42cafd710d2102ada7eb12e07b
Roach <urbandev@gmail.com>
2016-01-07 00:49:44 -0800

    Remove 'open flickr auth page' in lieu of printing the URL to console

--- photogame.rb
+++ photogame.rb
@@ -11,8 +11,8 @@ authenticated = false
 token = flickr.get_request_token
 auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'delete')
 # Print oauth url or open it in a browser window
-# puts "Open this url in your process to complete the authication process : #{auth_url}"
-system("open", auth_url)
+puts "Open this url in your process to complete the authication process : #{auth_url}"
+# system("open", auth_url)
 puts "Copy here the number given when you complete the process."
 verify = gets.strip
 
